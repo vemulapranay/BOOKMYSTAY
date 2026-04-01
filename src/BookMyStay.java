315c61d0670a9e2cdb2671304ce30dd4fb83faed
vemulapranay <vemulapranay76@email.com>
2026-04-01 14:38:54 +0530

    UC1:application and welcome message

---
+++ src/BookMyStay.java
@@ -0,0 +1,13 @@
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("        Welcome to Book My Stay     ");
+        System.out.println("   Hotel Booking Management System  ");
+        System.out.println("           Version 1.0              ");
+        System.out.println("====================================");
+
+        System.out.println("Application started successfully.");
+    }
+}
